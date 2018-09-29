5db93bbcdd68c2b7d2c3c2de3a3ca608818f3956
phanthai <phanthai.uit@gmail.com>
2018-09-29 14:57:16 +0700

    init source

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import Home from './components/views/home';
-import ExamDetail from './components/views/azure';
-import Result from './components/views/holder';
+import ExamDetail from './components/views/examdetail';
+import Result from './components/views/result';
 
 export default (
   <Route path='/' component={App}>
